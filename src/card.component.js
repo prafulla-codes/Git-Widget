d1c1c4ecc2f5550af422e6be1042f8ade5417027
Pika1998 <prafzzraichurkar1@gmail.com>
2020-02-16 22:44:23 +0530

    Created First Draft

--- src/card.component.js
+++ src/card.component.js
@@ -5,11 +5,18 @@ template.innerHTML=`
 <style>
 ${css}
 </style>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/js/fontawesome.min.js"></script>
+
 <div class="card">
-    <div class="header">
-        <h4 id="header-group-name"> </h4> 
+    <a id="group_link" target="_blank">
+    <div id="header">
+    <img src="./assets/meetup-brands.svg" id='logo'>
+    <h4 id="header-group-name"> </h4> 
     </div>
-    <div class="content">
+    </a>
+    <div id="content">
+    </div>
+    <div id="footer">
     </div>
 </div>   
 `
@@ -43,11 +50,36 @@ class MeetupCard extends HTMLElement {
         })
        
         let event_data = await events.json();
+        event_data = event_data.sort(function(a,b){
+            return new Date(b.local_date) - new Date(a.local_date);
+        })
         console.log(event_data)
-        
+        // Render The Events
+        this.renderEvents(event_data);
     }
 
+    renderEvents(events_data){
+        events_data.forEach(event=>{
 
+            let a = document.createElement('a');
+            a.setAttribute('href',event.link)
+            a.innerHTML=`
+            <div class="event">
+            <h4 style="text-align:left"> ${event.name} </h4>
+            <br>
+            <img src="./assets/calendar-alt-regular.svg" class="icon">&nbsp<span class="label">${this.reformatDate(event.local_date)}</span> <span class="label" style="float:right"><img src="./assets/user-check-solid.svg" class="icon"> ${event.yes_rsvp_count}+</span>
+            <div>
+            `
+            this._shadowRoot.querySelector(`#content`).appendChild(a);
+
+        })
+    }
+
+    reformatDate(dateStr)
+    {
+      let dArr = dateStr.split("-");  // ex input "2010-01-18"
+      return dArr[2]+ "-" +dArr[1]+ "-" +dArr[0] //ex out: "18/01/10"
+    }
     async render(){
         console.log("Rendering the meetup widget");
         let group = await fetch(`https://cors-anywhere.herokuapp.com/https://api.meetup.com/${this.dataset.groupurl}`,{
@@ -60,6 +92,7 @@ class MeetupCard extends HTMLElement {
         let group_data = await group.json();
         console.log(group_data)
         this._shadowRoot.querySelector(`#header-group-name`).innerHTML=group_data.name;
+        this._shadowRoot.querySelector(`#group_link`).setAttribute('href',group_data.link);
         this.fetchEvents();
         
     }
--- src/card.style.mjs
+++ src/card.style.mjs
@@ -1,24 +1,92 @@
 export const css = 
 `
+    @import url('https://fonts.googleapis.com/css?family=Roboto&display=swap');
     .card {
         box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);
-        width:var(--main-width,300px);;
+        width:var(--main-width,350px);;
         height:var(--main-height,500px);
-        background-color:var(--main-bgcolor,#0D455D);
+        background-color:var(--main-bgcolor,#F6F7F8);
+        font-family:'Roboto',sans-serif;
     }
     .card * {
         box-sizing:border-box;
         padding:0;
         margin:0;
     }
-    .header{
+    #logo {
+        display:inline-block;
+        width:40px;
+        height:40px;
+        vertical-align:'middle';
+
+    }
+    .icon{
+        width:15px;
+        height:15px;
+    }
+    .label{
+        font-weight:bold;
+        color:var(--label-color,#0D455D);
+    }
+    #header{
         width:100%;
         height:wrap;
-        padding:15px 10px;
+        padding:20px 25px;
         color:var(--header-color,#fff);
         font-weight:bold;
+        box-shadow: 0px 0px 5px 0px black;
         background-color:var(--header-bgcolor,#EE585F);
-
+    }
+    #footer{
+        width:100%;
+        height:30px;
+        background-color:var(--header-bgcolor,#EE585F);
+        font-weight:bold;
+        vertical-align:bottom;
+    }
+    #header-group-name{
+        display:inline-block;
+        vertical-align:top;
+        margin-top:15px;
+        margin-left:5px;
+    }
+    #content{
+        max-height:90%;
+        min-height:90%;
+        overflow-y:scroll;
+        padding:0;
+        padding-top:13px;
+        background-color:var(--content-bgcolor,#F6F7F8);
+    }
+    #content::-webkit-scrollbar {
+        width: 3px;
+    }
+     
+    #content::-webkit-scrollbar-track {
+        background-color: var(--scroll-track,#fff);
+    }
+    #content::-webkit-scrollbar-thumb {
+        background-color: var(--scroll-thumb,#EE585F);
+        border-radius: 5px;
+      }
+    a{
+        text-decoration:none;
+        color:black;
+    }
+    a:hover{
+       --event-bgcolor:black;
+       color:#fff;
+       --label-color:#EE585F;
+    }
+    .event{
+        width:90%;
+        padding:20px;
+        margin-left:auto;
+        margin-right:auto;
+        background: var(--event-bgcolor,#FFFFFF);
+        border: 1px solid #E8E8E8;
+        border-radius: 10px;
+        margin-bottom:13px;
     }
     h4{
         font-weight:800;
